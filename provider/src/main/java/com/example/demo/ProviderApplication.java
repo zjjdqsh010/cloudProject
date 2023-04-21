112af214199f230ef74b71f522c8a4fcc1397995
zjjdqsh010 <1248616974@qq.com>
2023-04-21 21:47:10 +0800

    0.0

--- provider/src/main/java/com/example/demo/ProviderApplication.java
+++ provider/src/main/java/com/example/demo/ProviderApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ProviderApplication {
 
+    //提供服务者
     public static void main(String[] args) {
         SpringApplication.run(ProviderApplication.class, args);
     }
